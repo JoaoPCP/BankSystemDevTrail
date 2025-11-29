5912ea9659d934b7ad6365db9b8e2acb2f0aa01c
João Paulo Cardoso da Paixão <67709427+JoaoPCP@users.noreply.github.com>
2025-11-28 21:01:06 -0300

    Revert "Revert "feat: listar contas por CPF do cliente""

---
+++ ProjetoDevTrail.Application/UseCase/Accounts/GetByClient/IGetAccountByClientCPFHandler.cs
@@ -0,0 +1,9 @@
+﻿using ProjetoDevTrail.Application.DTO.AccountDTO;
+
+namespace ProjetoDevTrail.Application.UseCase.Accounts.GetByClient
+{
+    public interface IGetAccountByClientHandler
+    {
+        Task<List<AccountInListViewDTO>> HandleAsync(Guid Id);
+    }
+}
--- ProjetoDevTrail.Infra/Repositories/AccountRepositories/IAccountRepository.cs
+++ ProjetoDevTrail.Infra/Repositories/AccountRepositories/IAccountRepository.cs
@@ -6,5 +6,6 @@ namespace ProjetoDevTrail.Infra.Repositories.AccountRepositories
     public interface IAccountRepository : IRepository<Account>
     {
         public Task<Account?> GetByAccountNumberAsync(string accountNumber);
+        public Task<List<Account>> GetByClientIdAsync(Guid clientId);
     }
 }
